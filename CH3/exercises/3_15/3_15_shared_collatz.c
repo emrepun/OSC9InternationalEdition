d0cd17f6189d614fe8f833e27db6ee02544ca2ab
emrepun <emrehavan@hotmail.com>
2021-01-30 22:11:33 +0100

    Implement shared memory communication 3_15

--- CH3/exercises/3_15/3_15_shared_collatz.c
+++ CH3/exercises/3_15/3_15_shared_collatz.c
@@ -7,22 +7,20 @@
 #include <sys/stat.h>
 #include <sys/mman.h>
 #include <ctype.h>
+#include <string.h>
 
 
-void collatz(int val) {
-  printf("%i\n", val);
-
-  if (val == 1) {
-    return;
+int collatz_step(int val) {
+  if (val == 1 || val == 0) {
+    return val;
   }
 
   if (val % 2 == 0) {
-    int newVal = val / 2;
-    collatz(newVal);
+    return val / 2;
   } else {
-    int newVal = (val * 3) + 1;
-    collatz(newVal);
+    return (val * 3) + 1;
   }
+
 }
 
 int main(int argc, char *argv[]) {
@@ -38,16 +36,38 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
+  const int SIZE = 4096;
+	const char *name = "OS";
+
+  int shm_fd;
+	void *ptr;
+
+  shm_fd = shm_open(name, O_CREAT | O_RDWR, 0666);
+
+  ftruncate(shm_fd, SIZE);
+
+  ptr = mmap(0, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+  if (ptr == MAP_FAILED) {
+    printf("Map failed\n");
+    return -1;
+  }
+
   pid_t pid;
 
   pid = fork();
 
   if (pid == 0) { // child process
     printf("CHILD\n");
-    collatz(val);
+    while (val != 1) {
+      val = collatz_step(val);
+      ptr+= sprintf(ptr, "%i", val);
+      ptr+= sprintf(ptr, " ");
+    }
   } else if (pid > 0) { // parent process
     wait(NULL);
     printf("PARENT\n");
+
+    printf("%s", ptr);
   }
 
 }
