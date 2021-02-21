bbf40874ea5c1e447b05f67842b8146599a676f9
emrepun <emrehavan@hotmail.com>
2021-02-21 22:57:23 +0300

    Use a better approach, so that random number generation is not serial and can work with multiple threads
    See the following for more details: https://codereview.stackexchange.com/a/256282/168876

--- CH4/exercises/4_18/openmp_approximate_pi.c
+++ CH4/exercises/4_18/openmp_approximate_pi.c
@@ -1,27 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <omp.h>
 
-double calculate_in_circle_random_count(void)
-{
-    static const unsigned total = 100000;
+typedef unsigned long long rand_state;
+
+// multiplier/modulus taken from Knuth Volume 2, Table 1
+static const int multiplier = 314159269;
+static const int addend = 1;
+static const int modulus = 0x7fffffff;
+
+#define OMP_RAND_MAX 0x7fffffff
+
+// note that this works differently from srand, returning a seed rather than setting
+// a hidden seed.
+rand_state omp_srand() {
+    rand_state state = time(NULL);
+    state ^= (unsigned long long)omp_get_thread_num() << 32;
+    return state;
+}
+
+int omp_rand(rand_state *state) {
+    *state = *state * multiplier + addend;
+    return *state & modulus;
+}
+
+double calculate_in_circle_random_count(void) {
+
+    static const unsigned total = 1000000;
     unsigned in_circle = 0;
 
-    #pragma omp parallel for reduction(+:in_circle)
+#pragma omp parallel reduction(+:in_circle)
     {
-      for (unsigned i = 0;  i < total;  ++i) {
-          double x = (double)rand() / RAND_MAX;
-          double y = (double)rand() / RAND_MAX;
+        // do per-thread initialization
+        rand_state seed = omp_srand();
+        int count = total / omp_get_num_threads();
+        int i;
 
-          double val = x * x + y * y;
-          in_circle +=  val < 1.0;
-      }
+        // then do this thread's portion of the computation:
+        for (i = 0;  i < count;  ++i) {
+
+            double x = (double)omp_rand(&seed) / OMP_RAND_MAX;
+            double y = (double)omp_rand(&seed) / OMP_RAND_MAX;
+
+            double val = x * x + y * y;
+            in_circle +=  val < 1.0;
+        }
     }
     return 4.0 * in_circle / total;
 }
 
-int main(void)
+int main(int argc, char const *argv[])
 {
-    const double pi_approx = calculate_in_circle_random_count();
+    int num_threads = 1;
+
+    if (argc > 1)
+        num_threads = atoi(argv[1]);
+
+    omp_set_num_threads(num_threads);
+
+    float pi_approx = calculate_in_circle_random_count();
+
     printf("Pi approximation: %.6f\n", pi_approx);
 
+    return 0;
 }
